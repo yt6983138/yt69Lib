e89a8471fe48be13812eba545f922fd255a976c5
yt6983138 <yt6983138@gmail.com>
2024-08-15 18:40:14 +0800

    add EnsureNotNull extension

---
+++ Common/Extensions.cs
@@ -0,0 +1,14 @@
+﻿namespace yt6983138.Common;
+public static class Extensions
+{
+	public static T EnsureNotNull<T>(this T? value) where T : class
+	{
+		ArgumentNullException.ThrowIfNull(value, nameof(value));
+		return value;
+	}
+	public static T EnsureNotNull<T>(this Nullable<T> value) where T : struct
+	{
+		ArgumentNullException.ThrowIfNull(value, nameof(value));
+		return value.Value;
+	}
+}
